ffff08daa4fc8a7e2aaf8938d5c73ddb1d23033e
Lee Hawkins <hotsaucehawkins@gmail.com>
2020-02-13 18:14:49 -0700

    change style.  change some redundant logic

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,8 +1,9 @@
-var words =["the", "word","say","root"];
+var words =["the", "word","say","root","this","that","start","end"];
 var wins = 0;
 var losses = 0;
 var wrongGuesses = [];
 var numberGuess = 0;
+var repeatLetter = false;
 var secretWord = words[Math.floor(Math.random()*words.length)];
 var arrayPlaceholder = [];
 
@@ -25,7 +26,9 @@ console.log(blankPlaceholder);
 
 window.onload = function(){
     var secretWordText=document.getElementById("secretword-text");
+    var numberText=document.getElementById("numguesses-text");
     secretWordText.textContent = this.blankPlaceholder;
+    numberText.textContent = 12;
 }
 
 
@@ -36,21 +39,25 @@ window.onload = function(){
 document.onkeyup = function(event) {
 
     var userGuess=event.key;
-    var beforeGuessPlaceholder=blankPlaceholder;
-    var afterGuessPlaceholder=beforeGuessPlaceholder;
-    console.log(wrongGuesses);
+    var afterGuessPlaceholder=blankPlaceholder;
+    
+    
     
 
     var userGuessText=document.getElementById("userguess-text");
     var directionsText=document.getElementById("directions-text");
     var secretWordText=document.getElementById("secretword-text");
     var wrongGuessText=document.getElementById("guesses-text");
+    var numberText=document.getElementById("numguesses-text");
     var lossText=document.getElementById("losses-text");
     var winText=document.getElementById("wins-text");
-
+    
+    const victory =document.getElementById("victorySound");
+    const loss =document.getElementById("losingSound");
     function resetVariables() {
         wrongGuesses = [];
         numberGuess = 0;
+        repeatLetter = false;
         secretWord = words[Math.floor(Math.random()*words.length)];
         arrayPlaceholder = [];
         for (var i=0; i<secretWord.length; i++){
@@ -62,7 +69,10 @@ document.onkeyup = function(event) {
         wrongGuessText.textContent = "";
         userGuessText.textContent = "";
         directionsText.textContent = "Press Any Key";
+        numberText.textContent = 12;
     }
+
+  
     
 
 
@@ -76,10 +86,18 @@ document.onkeyup = function(event) {
             console.log(afterGuessPlaceholder);
         } 
     }
-    if (beforeGuessPlaceholder===afterGuessPlaceholder) {
+    for (var i=0;i<wrongGuesses.length;i++) {
+        if (wrongGuesses[i]===userGuess) {
+            repeatLetter = true;
+
+        }
+    }
+    if (blankPlaceholder===afterGuessPlaceholder  && repeatLetter==false) {
         wrongGuesses.push(userGuess);
-        numberGuess=numberGuess +1;
+        numberGuess=numberGuess+1;
+        numberText.textContent= 12-numberGuess;
         if (numberGuess > 11) {
+            loss.play();
             alert("You lose!  The correct word was:   "  + secretWord);
             losses=losses+1;
             directionsText.textContent = "You lose!";
@@ -89,18 +107,19 @@ document.onkeyup = function(event) {
         } else {
             directionsText.textContent = "Choose again";
             wrongGuessText.textContent = wrongGuesses;
+            
 
         }
     } else if (secretWord==afterGuessPlaceholder) {
+        victory.play();
         wins=wins+1;
         directionsText.textContent = " You win!!!";
         secretWordText.textContent = afterGuessPlaceholder;
         alert("You win!  The correct word was:   " + secretWord);
-        userGuessText.textContent = "";
-        directionsText.textContent = "Press any key";
         winText.textContent= "Wins: " + wins;
+        
         resetVariables();
 
     } else {
-        beforeGuessPlaceholder=afterGuessPlaceholder;
+        repeatLetter = false;
     }}
